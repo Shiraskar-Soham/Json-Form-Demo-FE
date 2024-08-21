457281afbf65322c9827fdbb31f443cd1bca1974
Soham Shiraskar <soham.shiraskar@ofbusiness.in>
2024-08-21 22:45:17 +0530

    Minor css fix

--- src/dynamicCard/App.js
+++ src/dynamicCard/App.js
@@ -27,92 +27,82 @@ const jsonData = {
 
 function App() {
   return (
-    <div className="App">
-      <Card data={jsonData} />
-    </div>
-  );
-}
-
-function Card({ data }) {
-  return (
     <div className="card">
-      <HeaderSection header={data.header} />
-      <LabelChipsSection labelChips={data.labelChips} />
-      <BodySection body={data.body} />
-      <FooterSection footer={data.footer} />
+      <HeaderSection header={jsonData.header} />
+      <LabelChipsSection labelChips={jsonData.labelChips} />
+      <BodySection body={jsonData.body} />
+      <FooterSection footer={jsonData.footer} />
     </div>
   );
 }
 
 function HeaderSection({ header }) {
-    return (
-      <div className="card-header">
-        <h2>
-          {Object.values(header).join(' | ')}
-        </h2>
-        <span className='approvalButtons'>
-            <button
-              className="rejectButton"
-              // onClick={() => handleActionClick('REJECT')}
-              // hidden={accessRequestData.approvalStatus !== 'PENDING'}
-            >
-              Reject
-            </button>
-            <button
-              className="approveButton"
-              // onClick={() => handleActionClick('APPROVE')}
-              // hidden={accessRequestData.approvalStatus !== 'PENDING'}
-            >
-              Approve
-            </button>
-            <button
-              className="completeButton"
-              // onClick={() => handleActionClick('COMPLETE')}
-              // hidden={accessRequestData?.controlTowerStatus !== 'PENDING'}
-            >
-              Mark As Completed
-            </button>
-          </span>
+  return (
+    <div className="card-header">
+      <h2>
+        {Object.values(header).join(' | ')}
+      </h2>
+      <div className='approvalButtons'>
+        <button
+          className="rejectButton"
+        // onClick={() => handleActionClick('REJECT')}
+        // hidden={accessRequestData.approvalStatus !== 'PENDING'}
+        >
+          Reject
+        </button>
+        <button
+          className="approveButton"
+        // onClick={() => handleActionClick('APPROVE')}
+        // hidden={accessRequestData.approvalStatus !== 'PENDING'}
+        >
+          Approve
+        </button>
+        <button
+          className="completeButton"
+        // onClick={() => handleActionClick('COMPLETE')}
+        // hidden={accessRequestData?.controlTowerStatus !== 'PENDING'}
+        >
+          Mark As Completed
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 function LabelChipsSection({ labelChips }) {
-    return (
-      <div className="card-labelChips">
-        <div className="chips-container">
-          {Object.keys(labelChips).map((key) => (
-            <div key={key} className="chip">
-              <strong>{key}:</strong> {labelChips[key]}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
-
-  function BodySection({ body }) {
-    return (
-      <div className="card-body">
-        {Object.keys(body).map((key) => (
-          <div key={key}>
-            <p className="module-chips"><strong>{key}{': '}</strong>{body[key].join(', ')}</p>
+  return (
+    <div className="card-labelChips">
+      <div className="chips-container">
+        {Object.keys(labelChips).map((key) => (
+          <div key={key} className="chip">
+            <strong>{key}:</strong> {labelChips[key]}
           </div>
         ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+function BodySection({ body }) {
+  return (
+    <div className="card-body">
+      {Object.keys(body).map((key) => (
+        <div key={key}>
+          <span className="module-chips"><strong>{key}{': '}</strong>{body[key].join(', ')}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function FooterSection({ footer }) {
   return (
     <div className="card-footer">
-      <p>
         {Object.keys(footer).map((key) => (
           <span key={key}>
             <strong>{key}:</strong> {footer[key]}{('   ')}{ }
           </span>
         ))}
-      </p>
     </div>
   );
 }
